b8e7d312ddf7126a731762158db2bfd273d1eec1
Salai-Kowshikan <salaikowshikan8743@gmail.com>
2024-11-22 14:53:43 +0530

    Backend initiaized

---
+++ Backend/src/test/java/com/newsApp/newApp/NewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsApp.newApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
