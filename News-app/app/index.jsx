1bab492d6afac4f553aaa4a5a38af03635e6b141
Kuzon2004 <152062286+Kuzon2004@users.noreply.github.com>
2024-11-23 16:23:54 +0530

    frontned done

--- News-app/app/index.jsx
+++ News-app/app/index.jsx
@@ -4,40 +4,7 @@ import { useState, useEffect } from "react";
 import { Button, Text } from "react-native-paper";
 
 export default function Index() {
-  const [name, setName] = useState("Thiyanesh");
-  const [welcome, setWelcome] = useState("");
-
-  useEffect(() => {
-    if (name === "Thiyanesh") {
-      setWelcome("I am batman");
-    } else {
-      setWelcome("I am a chill guy");
-    }
-  });
-
-  return (
-    <>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>{name}</Text>
-        <Button
-          onPress={() => {
-            if (name === "Thiyanesh") {
-              setName("Salai");
-            } else {
-              setName("Thiyanesh");
-            }
-          }}
-        >
-          Change name
-        </Button>
-        <Text>{welcome}</Text>
-      </View>
+   return( <>
       <BottomBar />
     </>
   );
--- News-app/components/NewsPage.jsx
+++ News-app/components/NewsPage.jsx
@@ -1,36 +1,123 @@
-import React from "react";
-import {View} from "react-native";
-import { useState, useEffect } from "react";
-import {Button,Card,Text} from "react-native-paper"
-import { IconButton, MD3Colors } from 'react-native-paper';
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet, Image, ScrollView } from "react-native";
+import { Button, Card, Text, IconButton, Avatar } from "react-native-paper";
+import axios from "axios";
 
 const NewsPage = () => {
-    const [save,setSave]=useState(0)
+  const [saved, setSaved] = useState(false);
+  const [response, setResponse] = useState([
+    {
+      title: "Thiyanesh got married",
+      desc: "He pulled the best girl",
+      image: "https://picsum.photos/700",
+    },
+    {
+      title: "Thiyanesh is the richest person on Earth",
+      desc: "but his wife is divorcing him",
+      image: "https://picsum.photos/700",
+    },
+    {
+      title: "Thiyanesh gave up his wealth to charity",
+      desc: "he took refuge in a monastry in nepal",
+      image: "https://picsum.photos/700",
+    },
+  ]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const response = await axios.get("https://cat-fact.herokuapp.com/facts/");
+      console.log(response.data);
+    };
+    fetchAPI();
+  }, []);
   return (
-    <View>
-    <Card>
-    <View
-        style={{
-          flex: 1,
-          justifyContent: "flex-start",
-          alignItems: "flex-end",
-        }}
-      >
-    
-    <IconButton icon='bookmark-check-outline' type = 'contained'
-    onPress={() => console.log('Pressed')} /></View>
-    <Card.Content>
-      <Text variant="titleLarge">News-1</Text>
-    </Card.Content>
-    <Card.Content>
-      <Text variant="bodyMedium">News Content</Text>
-    </Card.Content>
-    <Card.Actions>
-      <Button>View</Button>
-    </Card.Actions>
-  </Card>
-  </View>
+    <>
+      <ScrollView>
+        {response.map((res) => {
+          return (
+            <View style={pages.container}>
+              <Card style={Cards.container}>
+                <Card.Cover source={{ uri: res.image }} />
+                <Card.Title title={res.title} />
+                <Card.Content>
+                  <Text>{res.desc}</Text>
+                </Card.Content>
+                <Card.Actions>
+                  <IconButton
+                    style={Cards.save}
+                    icon={saved ? "bookmark-check" : "bookmark-check-outline"}
+                    onPress={() => {
+                      setSaved(!saved);
+                    }}
+                  ></IconButton>
+                  <Button style={Cards.ViewB}>View</Button>
+                </Card.Actions>
+              </Card>
+            </View>
+          );
+        })}
+      </ScrollView>
+    </>
+    // <>
+    //   <View style={styles.container}>
+    //     <Card style={styles.newsCard}>
+    //       <Card.Cover
+    //         style={styles.image}
+    //         source={{ uri: "https://reactnative.dev/img/tiny_logo.png" }}
+    //       />
+    //       <Card.Title title="Title" subtitle="Description" />
+    //       <Card.Content>
+    //         <Text style={styles.description}>Description here</Text>
+    //       </Card.Content>
+    //       <Card.Actions>
+    //         <IconButton
+    //           icon={saved ? "bookmark-check" : "bookmark-outline"}
+    //           type="contained"
+    //           onPress={() => {
+    //             setSaved(!saved);
+    //           }}
+    //         />
+    //         <Button> View </Button>
+    //       </Card.Actions>
+    //     </Card>
+    //   </View>
+    // </>
   );
 };
 
+// const styles = StyleSheet.create({
+//   container: {
+//     display: "flex",
+//     flex: 1,
+//     flexDirection: "column",
+//     width: "100%",
+//     marginTop: "16",
+//     marginHorizontal: "20",
+//   },
+//   image: {
+//     marginHorizontal: "20",
+//   },
+//   newsCard: {
+//     width: "90%",
+//     padding: "10",
+//   },
+//   description: {
+//     flex: 1,
+//   },
+// });
+
+const pages = StyleSheet.create({
+  container: {
+    display: "flex",
+    flex: 1,
+    padding: "5%",
+    flexDirection: "column",
+  },
+});
+const Cards = StyleSheet.create({
+  container: {
+    display: "flex",
+    flex: "1",
+  },
+});
 export default NewsPage;
